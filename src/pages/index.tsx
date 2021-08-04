a47db1b1dd4745f474f89d504380149ae4e7e000
Leandro Guaraldi <leandro_319@yahoo.com.br>
2021-08-04 00:53:09 -0300

    teste header ok

---
+++ src/pages/index.tsx
@@ -0,0 +1,42 @@
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { getPrismicClient } from '../services/prismic';
+import commonStyles from '../styles/common.module.scss';
+import styles from './home.module.scss';
+
+interface Post {
+  uid?: string;
+  first_publication_date: string | null;
+  data: {
+    title: string;
+    subtitle: string;
+    author: string;
+  };
+}
+
+interface PostPagination {
+  next_page: string;
+  results: Post[];
+}
+
+interface HomeProps {
+  postsPagination: PostPagination;
+}
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <h1>teste</h1>
+    </>
+  );
+}
+
+// export const getStaticProps = async () => {
+//   const prismic = getPrismicClient();
+//   console.log(prismic);
+//   // const postsResponse = await prismic.query(TODO);
+//   // TODO
+// };
